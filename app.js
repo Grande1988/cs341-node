ffbf6e53079242aa4aebbdaf55c9e6954e00f99c
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-13 20:26:12 -0600

    Revert "added more app.js"
    This reverts commit 3c4c68554d0ccfa08aba59fa4c772aa241d5d7d7.

--- app.js
+++ app.js
@@ -16,21 +16,5 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const options = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4
+
 };
-
-const MondoDB_URL = process.env.MondoDB_URL || 'mongodb+srv://reddgl:Grande!1988@cluster0.sll31.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views', 'views');
-
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded((extended: false)));
