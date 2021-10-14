3c1e14dc252ddc6fc08d61764e62cd3137203444
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-13 20:26:40 -0600

    Revert "message"
    This reverts commit afac4ea0a0c8e27eafa0da425bfa029c5f1cbb97.

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Assignment 1</title></head>');
-    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
+    );
     res.write('</html>');
     return res.end();
   }
