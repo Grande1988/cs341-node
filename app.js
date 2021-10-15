44708b4459e7723c0af6b46363e076657e4a4b83
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-15 14:43:55 -0600

    finished academind section

--- app.js
+++ app.js
@@ -1,37 +1,79 @@
-const http = require('http');
-
-const server = http.createServer((req, res) => {
-  const url = req.url;
-  if (url === '/') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/users') {
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>Assignment 1</title></head>');
-    res.write('<body><ul><li>User 1</li><li>User 2</li></ul></body>');
-    res.write('</html>');
-    return res.end();
-  }
-  // Send a HTML response with some "Page not found text
-  if (url === '/create-user') {
-    const body = [];
-    req.on('data', chunk => {
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody.split('=')[1]); // username=whatever-the-user-entered
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    res.end();
-  }
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+const errorController = require("./controllers/error");
+const User = require("./models/user");
+const MONGODB_URI =
+  "mongodb+srv://reddgl:Grande!1988@cluster0.sll31.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
 });
 
-server.listen(3000);
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const authRoutes = require("./routes/auth");
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: "my secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
+);
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+app.use(authRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(MONGODB_URI)
+  .then((result) => {
+    User.findOne().then((user) => {
+      if (!user) {
+        const user = new User({
+          name: "Jen",
+          email: "jen@test.com",
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
