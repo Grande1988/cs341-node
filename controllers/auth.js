23dc829106af3cf38ea963d9804d2c2bbea58d37
Grande1988 <71180666+Grande1988@users.noreply.github.com>
2021-10-19 22:22:40 -0600

    auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -11,6 +11,12 @@ const transporter = nodemailer.createTransport(sendgridTransport({
 }));
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
